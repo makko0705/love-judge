548ff6dc3ddf1d34f792d971ad8f1ab57bc5df45
makko0705 <urban-pirates@live.jp>
2024-07-10 22:42:36 +0900

    detail 見た目の調整のためhtml変更

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container page-detail">
 
-    <h1>{{ $consultation->partner_name }} さんの診断詳細</h1>
+    {{-- <h1>{{ $consultation->partner_name }} さんの診断詳細</h1> --}}
     @if ($consultation->diagnoses->isNotEmpty())
         @php
             $diagnosis = $consultation->diagnoses->first();
@@ -15,9 +15,9 @@
             <img id="resultImage" src="{{ asset('images/' . ($diagnosisContent['GOorWAIT'] === 'GO' ? 'GO.png' : 'WAIT.png')) }}" alt="結果画像">
         </div>
 
-        <div>{{ $consultation->partner_name }}さんとの恋愛可能性: {{ $diagnosisContent['恋愛可能性'] }}</div>
+        <div id="compatibilityArea">{{ $consultation->partner_name }}さんとの恋愛可能性: {{ $diagnosisContent['恋愛可能性'] }}</div>
         {{-- <div>GOorWAIT: {{ $diagnosisContent['GOorWAIT'] }}</div> --}}
-        <div>診断結果: {{ $diagnosisContent['診断結果'] }}</div>
+        <div>{{ $diagnosisContent['診断結果'] }}</div>
 
     @else
         <div>No diagnosis available</div>
